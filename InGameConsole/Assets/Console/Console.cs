fb993b5a74372f32320169494a62de6155d52c78
pol <paulnoblot@gmail.com>
2019-02-07 14:02:15 +0100

    Adding console prefab ClearConsole button InputField gestion

--- InGameConsole/Assets/Console/Console.cs
+++ InGameConsole/Assets/Console/Console.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace GameConsole
@@ -17,7 +18,7 @@ namespace GameConsole
 
         private static GameObject gameobjectNewText;
 
-        private static Console singleton;
+        // private static Console singleton;
 
 
         private void Awake()
@@ -29,53 +30,75 @@ namespace GameConsole
             minusButton = transform.Find("Buttons").Find("MinusButton").GetComponent<Button>();
             clearButton = transform.Find("Buttons").Find("ClearButton").GetComponent<Button>();
             gameobjectNewText = transform.Find("NewText").gameObject;
+
+            clearButton.onClick.AddListener(ClearConsole);
         }
 
 
         public static void Write(string _message, Color _color)
         {
-            if (!singleton)
-                singleton = new Console();
+            // if (!singleton)
+            //     singleton = new Console();
          
             GameObject newText = Instantiate(gameobjectNewText, consoleContent);
             newText.GetComponent<Text>().color = _color;
             newText.GetComponent<Text>().text = "[" + System.DateTime.UtcNow.ToString("HH:mm:ss") + "] " + _message;
-            newText.transform.SetAsFirstSibling();
+            //newText.transform.SetAsFirstSibling();
 
             // width = 580
 
             //consoleContent.GetComponent<RectTransform>().sizeDelta = new Vector2(0, consoleContent.childCount * 30);
         }
+        
+        private void Update()
+        {
+            if (EventSystem.current.currentSelectedGameObject == consoleContent.gameObject)
+            {
+                input.Select();
+                input.ActivateInputField();
+            }
 
+            if (EventSystem.current.currentSelectedGameObject == input.gameObject && Input.GetKeyDown(KeyCode.Return))
+            {
+                Send();
+            }
 
-
+            if (Input.GetKeyDown(KeyCode.F1))
+            {
+                input.Select();
+                input.ActivateInputField();
+            }
+        }
+        
         public static void Write(string _message)
         {
-            if (!singleton)
-                singleton = new Console();
-
             Write(_message, new Color(0.12f, 0.12f, 0.12f));
         }
-
-
-
+        
         public static void Warning(string _message)
         {
-            if (!singleton)
-                singleton = new Console();
             Write(_message, Color.yellow);
         }
 
         public static void Error(string _message)
         {
-            if (!singleton)
-                singleton = new Console();
             Write(_message, Color.red);
         }
 
         private static void Send()
         {
+            Write(input.text);
+            input.text = string.Empty;
+            input.Select();
+            input.ActivateInputField();
+        }
 
+        public static void ClearConsole()
+        {
+            foreach (Transform child in consoleContent.transform)
+            {
+                Destroy(child.gameObject);
+            }
         }
     }
 }
--- InGameConsole/Assets/Test.cs
+++ InGameConsole/Assets/Test.cs
@@ -13,19 +13,19 @@ public class Test : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.F2))
         {
             GameConsole.Console.Write("this is a message");
         }
-        if (Input.GetKeyDown(KeyCode.Z))
+        if (Input.GetKeyDown(KeyCode.F3))
         {
             GameConsole.Console.Warning("this is a warning");
         }
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.F4))
         {
             GameConsole.Console.Error("this is an error");
         }
-        if (Input.GetKeyDown(KeyCode.R))
+        if (Input.GetKeyDown(KeyCode.F5))
         {
             GameConsole.Console.Write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
         }
