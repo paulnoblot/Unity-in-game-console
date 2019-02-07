f757e43b55fbb2453370339bd9055a820d47f623
pol <paulnoblot@gmail.com>
2019-02-07 08:33:04 +0100

    Initial commit

---
+++ InGameConsole/Assets/Console/Console.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameConsole
+{
+    public class Console : MonoBehaviour
+    {
+        [SerializeField]
+        private static Transform consoleContent;
+        private static InputField input;
+        private static Button enterButton;
+        private static Button exitButton;
+        private static Button minusButton;
+        private static Button clearButton;
+
+        private static GameObject gameobjectNewText;
+
+        private static Console singleton;
+
+
+        private void Awake()
+        {
+            consoleContent = transform.Find("ScrollView").Find("Viewport").Find("Content");
+            input = transform.Find("InputField").GetComponent<InputField>();
+            enterButton = transform.Find("EnterButton").GetComponent<Button>();
+            exitButton = transform.Find("Buttons").Find("ExitButton").GetComponent<Button>();
+            minusButton = transform.Find("Buttons").Find("MinusButton").GetComponent<Button>();
+            clearButton = transform.Find("Buttons").Find("ClearButton").GetComponent<Button>();
+            gameobjectNewText = transform.Find("NewText").gameObject;
+        }
+
+
+        public static void Write(string _message, Color _color)
+        {
+            if (!singleton)
+                singleton = new Console();
+         
+            GameObject newText = Instantiate(gameobjectNewText, consoleContent);
+            newText.GetComponent<Text>().color = _color;
+            newText.GetComponent<Text>().text = "[" + System.DateTime.UtcNow.ToString("HH:mm:ss") + "] " + _message;
+            newText.transform.SetAsFirstSibling();
+
+            // width = 580
+
+            //consoleContent.GetComponent<RectTransform>().sizeDelta = new Vector2(0, consoleContent.childCount * 30);
+        }
+
+
+
+        public static void Write(string _message)
+        {
+            if (!singleton)
+                singleton = new Console();
+
+            Write(_message, new Color(0.12f, 0.12f, 0.12f));
+        }
+
+
+
+        public static void Warning(string _message)
+        {
+            if (!singleton)
+                singleton = new Console();
+            Write(_message, Color.yellow);
+        }
+
+        public static void Error(string _message)
+        {
+            if (!singleton)
+                singleton = new Console();
+            Write(_message, Color.red);
+        }
+
+        private static void Send()
+        {
+
+        }
+    }
+}
---
+++ InGameConsole/Assets/Test.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GameConsole.Console.Write("this is a message");
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameConsole.Console.Warning("this is a warning");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameConsole.Console.Error("this is an error");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GameConsole.Console.Write("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+        }
+    }
+}
